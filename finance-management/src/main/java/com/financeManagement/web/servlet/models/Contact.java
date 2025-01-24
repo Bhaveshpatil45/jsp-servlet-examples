35dea5aa39b3f11374b50f7639c04ce39f143df8
Bhaveshpatil45 <bp700259@gmail.com>
2025-01-24 15:45:44 +0530

    finance-management

--- finance-management/src/main/java/com/financeManagement/web/servlet/models/Contact.java
+++ finance-management/src/main/java/com/financeManagement/web/servlet/models/Contact.java
@@ -1,51 +1,17 @@
 package com.financeManagement.web.servlet.models;
 
+import lombok.*;
 
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Contact {
     private int id;
     private Address address;
     private String email;
     private String mobileNo;
 
-    public Contact(int id, Address address, String email, String mobileNo) {
-        this.id = id;
-        this.address = address;
-        this.email = email;
-        this.mobileNo = mobileNo;
-    }
-
-    // Getters and setters...
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getMobileNo() {
-        return mobileNo;
-    }
-
-    public void setMobileNo(String mobileNo) {
-        this.mobileNo = mobileNo;
-    }
 }
 
