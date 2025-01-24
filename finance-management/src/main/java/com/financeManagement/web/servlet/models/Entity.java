6ef837f6753681953f2a73a646b23d5dc3a304b3
Bhaveshpatil45 <bp700259@gmail.com>
2025-01-24 15:46:20 +0530

    finance-management

--- finance-management/src/main/java/com/financeManagement/web/servlet/models/Entity.java
+++ finance-management/src/main/java/com/financeManagement/web/servlet/models/Entity.java
@@ -1,5 +1,13 @@
 package com.financeManagement.web.servlet.models;
 
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Entity {
     private int id;
     private String name;
@@ -7,52 +15,4 @@ public class Entity {
     private String type; // e.g., "User"
     private String state; // e.g., "Active" or "Inactive"
 
-    public Entity(int id, String name, Contact contact, String type, String state) {
-        this.id = id;
-        this.name = name;
-        this.contact = contact;
-        this.type = type;
-        this.state = state;
-    }
-
-    // Getters and Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Contact getContact() {
-        return contact;
-    }
-
-    public void setContact(Contact contact) {
-        this.contact = contact;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
 }
