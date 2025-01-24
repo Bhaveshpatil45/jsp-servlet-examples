18d62823c5f2d18c342428da7adc41554a821ec3
Bhaveshpatil45 <bp700259@gmail.com>
2025-01-24 15:46:58 +0530

    finance-management

--- finance-management/src/main/java/com/financeManagement/web/servlet/models/Notification.java
+++ finance-management/src/main/java/com/financeManagement/web/servlet/models/Notification.java
@@ -1,7 +1,15 @@
 package com.financeManagement.web.servlet.models;
 
+import lombok.*;
+
 import java.util.Date;
 
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Notification {
     private int id;
     private String message;
@@ -9,52 +17,4 @@ public class Notification {
     private Date date;
     private User user;
 
-    public Notification(int id, String message, String type, Date date, User user) {
-        this.id = id;
-        this.message = message;
-        this.type = type;
-        this.date = date;
-        this.user = user;
-    }
-
-    // Getters and Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
 }
