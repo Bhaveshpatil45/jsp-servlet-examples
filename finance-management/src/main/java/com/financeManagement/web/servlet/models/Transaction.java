7234a05014fd89da4d3e6e5a98e4be2ffabcf3ce
Bhaveshpatil45 <bp700259@gmail.com>
2025-01-24 15:49:09 +0530

    finance-management

--- finance-management/src/main/java/com/financeManagement/web/servlet/models/Transaction.java
+++ finance-management/src/main/java/com/financeManagement/web/servlet/models/Transaction.java
@@ -1,7 +1,10 @@
 package com.financeManagement.web.servlet.models;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class Transaction {
     private int id;
     private double amount;
@@ -11,70 +14,4 @@ public class Transaction {
     private String description;
     private User user;
 
-    public Transaction(int id, double amount, String type, String category, Date date, String description, User user) {
-        this.id = id;
-        this.amount = amount;
-        this.type = type;
-        this.category = category;
-        this.date = date;
-        this.description = description;
-        this.user = user;
-    }
-
-    // Getters and setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
 }
