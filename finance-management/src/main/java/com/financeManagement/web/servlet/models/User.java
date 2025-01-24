ff28cbb06941cb959cdb14b92590a93c8b6414f1
Bhaveshpatil45 <bp700259@gmail.com>
2025-01-24 15:49:49 +0530

    finance-management

--- finance-management/src/main/java/com/financeManagement/web/servlet/models/User.java
+++ finance-management/src/main/java/com/financeManagement/web/servlet/models/User.java
@@ -1,5 +1,9 @@
 package com.financeManagement.web.servlet.models;
 
+
+import lombok.Data;
+
+@Data
 public class User {
     private int id;
     private String username;
@@ -7,62 +11,4 @@ public class User {
     private String type; // User, Admin
     private Contact contact;
     private String state; // Active, Inactive
-
-    public User(int id, String username, String password, Contact contact, String type, String state) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.contact = contact;
-        this.type = type;
-        this.state = state;
-    }
-
-    // Getters and setters...
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Contact getContact() {
-        return contact;
-    }
-
-    public void setContact(Contact contact) {
-        this.contact = contact;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
 }
